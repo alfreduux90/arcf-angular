10be0b8fdce28b9b1dec872cd6e00f53963fdc5d
alfreduux90 <131402002+alfreduux90@users.noreply.github.com>
2023-08-12 21:37:04 -0500

    3 files modified, 46 added, 1 deleted

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+
+import { MaterialModule } from "./material.module";
+
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
