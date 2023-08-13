f9c325e420d5ed66dcadde8a21ecd912dfa12af1
alfreduux90 <131402002+alfreduux90@users.noreply.github.com>
2023-08-13 16:06:45 -0500

    4 files modified, 3 deleted

--- src/app/shared/components/organisms/organisms.module.ts
+++ src/app/shared/components/organisms/organisms.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { NgStyle } from '@angular/common';
-import { MaterialModule } from 'src/app/material.module';
+import { MaterialModule } from '../../../material.module';
 
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
